f82e654fb4fc5c71a9471390418190907f868004
Liangquan Zhou <Liangquan Zhou>
2015-11-18 18:18:48 -0500

    finish the attorney database update

--- data_import.R
+++ data_import.R
@@ -123,31 +123,33 @@ d2s = d2s[!duplicated(d2s[,"B"]),]
  
  # output all NAs in d2s$D, which are attorneys need to be updated
  d2s1 = d2s[is.na(d2s$D),]
- d2s1 = d2s1[!duplicated(d2s1[,"C"]),]
- write.table(d2s1, 'update attorney.csv', col.names = F, row.names = F, sep = ",")
-
+ d2s12 = d2s1[!duplicated(d2s1[,"C"]),] # contains no duplicates
+ d2s12 = drop_and_rename(d2s12, c("A", "B"))
+ write.table(d2s12, 'update attorney.csv', col.names = F, row.names = F, sep = ",")
+ 
+################################################################################
+# then manually update the attorney info at update attorney.csv file and save it
+################################################################################
 ## fix the attorney table and update it in the access database
  
  d2s2 = read.table(file = 'update attorney.csv',sep = ',')
- d2s2 = drop_and_rename(d2s2, c("A","B"))
+ d2s2 = drop_and_rename(d2s2)
  names(d2s2) = c("Wrong Attorney","Right Attorney")
 # d2s2$ID = seq(from = length(List$ID)+1,length.out = length(d2s2$ID))
+
+ sqlQuery(con, "create table temp(ID int, `Wrong Attorney` varchar(255),`Right Attorney` varchar(255), PRIMARY KEY (ID));")
+ sqlSave(con, d2s2, tablename = "temp")
  
- d2s3 = rbind(d2s2,List)
+ sqlQuery(con, "insert into List (`Wrong Attorney`, `Right Attorney`) select WrongAttorney,RightAttorney from temp;")
+ sqlQuery(con, 'drop table temp;')
+# now the database has been updated and now update the corresponding Right Attorney in d2s
+
+ List <- sqlFetch(con, "List")
+ List$`Wrong Attorney` = trimws(List$`Wrong Attorney`)
+ List$`Right Attorney` = trimws(List$`Right Attorney`)
+ a = which(d2s$C %in% List$`Wrong Attorney`)
+ d2s$D[a] = as.character(sapply(d2s$C[a], f))
  
- 
- 
- sqlSave(con, d2s2, tablename = "List", rownames = F, colnames = F, addPK = T, append = T, safer = T)
- 
- sqlUpdate(con,d2s2, tablename = "List", index = c("Wrong Attorney","Right Attorney"))
- 
- sqlQuery(con, 'insert into List (`Wrong Attorney`, `Right Attorney`) select WrongAttorney, RightAttorney from test1' )
- sqlQuery(con, 'insert into List select * from test1')
- 
- bb = sqlQuery(con, 'select * from List union select * from test1')
- sqlUpdate(con, d2s3, tablename = "List")
- 
-View(bb) 
 write.table(d2s, file = "Attorney.csv", sep = ',', row.names = F, col.names = F)
 
 ################# APN #########################
