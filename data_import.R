74374ca0f8b2a696238fa088255648cf94234792
Liangquan Zhou <Liangquan Zhou>
2015-11-24 16:59:44 -0500

    clean block and lot

--- data_import.R
+++ data_import.R
@@ -233,9 +233,12 @@ write.xlsx(d3sEnew, file = "APNcolumnE.xlsx", col.names = F, row.names = F)
 d3sEgood = read.xlsx(file = "APNcolumnE.xlsx", sheetIndex = "Sheet1", header = F)
 d3sEgood = drop_and_rename(d3sEgood)
 d3s = rbind(d3sEgood, d3s2)
-d3s = data.frame(d3s, stringAsFactor = F)
+d3s = data.frame(d3s, stringsAsFactors =  F)
 
 # now the column E should be all NAs
+
+################################################################################
+## with address and muni code #############################
 
 ## then update the county and town names and muni code
 
@@ -254,6 +257,8 @@ str(Key)
 Key = as.data.frame(apply(Key, 2, trimws))
 Key1 = Key[,c(2,5)]
 Key1 = unique(Key1)
+
+d3s1 = d3s
 
 # update the attorney name on d2s
 # which records in d2s in already in the List
@@ -278,12 +283,12 @@ bb = as.character(apply(d3s[,c("A","F")],f1))
 #replace those records in d2s
 d2s$D = NA
 
-f = function(x){
-  n = which(List$`Wrong Attorney` == x)
-  x = List$`Right Attorney`[n] 
-  return(x)
+f1 = function(x){
+  n = which(Key1[,1] == x)
+  x1 = Key1[n,2]
+  return(x1)
 }
-d2s$D[a] = as.character(sapply(d2s$C[a], f))
+d3s1$A = as.character(sapply(d3s1$A, f1))
 
 ## combine D E F as address
 head(d3s)
@@ -291,24 +296,44 @@ c1 = paste(paste(d3s$D, ifelse(!is.na(d3s$E), d3s$E, ''),sep = ''), d3s$F, 'NJ',
 d3s$c1 = c1
 c1s = c1[1:500]
 
-c1 = paste(d3s$F,'NJ',sep = ",")
+c1 = paste(d3s1$F,d3s1$A,'NJ',sep = ",")
 c1s = c1[1:500]
-add = geocode(c1s,output = 'more',source = 'google')
-addsub = add[,c("locality","administrative_area_level_2")]
+c2s = c1[501:1000]
+add2 = geocode(c2s,output = 'more',source = 'google')
+addsub2 = add2[,c("locality","administrative_area_level_2")]
+
+addsub3 = rbind(addsub,addsub2)
 
 View(add)
-View(cbind(addsub,d3s[1:500,c("A","F")]))
+
+sample = cbind(addsub3,d3s1[1:1000,c("A", "D", "E", "F","J")],test[1:1000,])
+write.xlsx(sample, file = "sample.xlsx", row.names = F, showNA = F)
+
+m = as.numeric(as.character(d3s1$J))
+
+munipath = "C:/Users/Liangquan/Google Drive/GovPilot/Teams/Data Aggregation _ Integrity Team/New Jersey/Data Import/Required Files/muni.xlsm"
+muni = read.xlsx(file = munipath, sheetIndex = "Sheet2", header = F,stringsAsFactors = F)
+View(muni)
+
+f2 = function(x){
+  n = which(muni[,3] == x)
+  return(muni[n,c(1,2)])
+}
+
+test1 = data.frame(sapply(m,f2))
+test = t(test1)
+
+View(cbind(test, d3s1[,c("A", "D", "E", "F","J")]),)
+
+# OAKLAND BORO,Bergen,nj
 
 
-
-OAKLAND BORO,Bergen,nj
-
-
-
-
-
-
-
+sample1 = sample[which(is.na(sample$locality)),]
+notgood = paste(sample1$D,sample1$A,'NJ',sep = ",")
+add4 = geocode(notgood,output = 'all',source = 'dsk')
+View(addsub4)
+addsub4 = add4[,c("locality","administrative_area_level_2")]
+View(cbind(addsub4,notgood))
 
 
 
@@ -358,14 +383,52 @@ write.table(apn_bad, file = "apn_bad.csv", sep = ',', row.names = F, col.names =
 
 
 ###############################################################
+### hold it, clean the block and lot first...
 ###############################################################
 
 ### load mulicipalities code data ### 
 
-d3s$D = d3s$c1
-drops = c('E','F','c1')
-d3s = d3s[,!(names(d3s) %in% drops)]
-head(d3s1)
-names(d3s) = LETTERS[1:length(names(d3s))]
+block = d3s$G
+lot = d3s$H
+
+block1 = gsub("(((FKA)|(F/K/A)|(QUAL)|(AKA)|(A/K/A)|(FK)|(F/K)|(AND))(.*))|,|(\\ )","",block)
+block2 = trimws(block1)
+block2 = gsub(" ",".",block2)
+View(cbind(as.character(block),block2))
+
+View(cbind(as.character(lot),lot9))
+lotpattern1 = "((FKA.*)|(F/K/A.*)|(AKA.*)|(A/K/A.*)|(FK.*)|(F/K.*)|(AND.*)|(&.*)|(L.*)|(THRU.*))|(QUAL)|(Q)|(Q:)|(:)|(\\()|(\\))"
+lotpattern2 = "(,)(.*[C]{1,}.*)" # clean format ,C****
+lotpattern3 = "(,)(.*[^C].*)"   # clean format , ^C
+lotpattern4 = "(\\S*)(\\s*)([C])(\\s*)(.*)"  #clean format XXX space C space XXX
+lotpattern5 = "(\\w)(\\s+)([0])"  # clean format XXX space 0number
+lotpattern6 = "(\\d+\\b)(\\s+)(\\d+\\b)(\\s+)(\\d+\\b)(.*)" ## clean format number space number space number ...
+lotpattern7 = "(\\d+)(\\s*)(-)(\\s*)(\\d+)" ## clean space
+lotpattern8 = "(\\d+)(\\s*)(-)(\\s*)(\\D+)"
+lotpattern9 = "(\\S*)(\\s*)([C].*)" 
+
+lot1 = gsub(lotpattern1, "", lot)
+lot2 = gsub(lotpattern2, "\\2", lot1)
+lot3 = gsub(lotpattern3, " ", lot2)
+lot4 = gsub(lotpattern4, '\\1 \\3\\5',lot3)
+lot5 = gsub(lotpattern5, '\\1.\\3',lot4)
+lot6 = gsub(lotpattern6, "\\1",lot5)
+lot7 = gsub(lotpattern7, "\\1", lot6)
+lot8 = gsub(lotpattern8, "\\1 \\5", lot7)
+lot9 = gsub(lotpattern9, '\\1_\\3', lot8)
 
 
+munipath = "C:/Users/Liangquan/Google Drive/GovPilot/Teams/Data Aggregation _ Integrity Team/New Jersey/Data Import/Required Files/muni.xlsm"
+muni = read.xlsx(file = munipath, sheetIndex = "Sheet2", header = F,stringsAsFactors = F)
+View(muni)
+m = as.numeric(as.character(d3s$J))
+f2 = function(x){
+  n = which(muni[,3] == x)
+  return(muni[n,c(1,2)])
+}
+
+test1 = data.frame(sapply(m,f2))
+muniadd = t(test1)
+View(muniadd)
+output = cbind(as.character(d3s$C),as.character(d3s$B),as.character(d3s$J),block2,lot9, paste(d3s$D,muniadd[,2],muniadd[,2], sep = ","))
+View(output)
