87c30a714aeadfa5d97228578e9b357967adcc07
Liangquan Zhou <Liangquan Zhou>
2015-12-07 18:29:34 -0500

    rewrite searching function

--- apn new.R
+++ apn new.R
@@ -6,20 +6,22 @@
 setwd("C:/Users/Liangquan/Desktop/tax data")
 
 filename = "Atlantic15.txt"
+con = file(filename)
 
 # read the txt file, return a dataframe
 f = function(x) {
-  x1 = substring(x, start, end)
+  x1 = substring(x, startnum, endnum)
   names(x1) = name
   return(x1)
 }
 
+startnum = c(1, 5, 10, 14, 19, 23, 59, 176, 522, 527, 531, 536, 540)
+endnum = c(4, 9, 13, 18, 22, 33, 83, 210, 526, 530, 535, 539, 550)
+name = c("muni", "blk", "blk.s", "lot", "lot.s", "qual", "address", "owner", "p.blk","p.blk.s","p.lot", "p.lot.s","p.qual")
+
+
 output = function(filename) {
   # create a function to truncate the string with
-  
-  start = c(1, 5, 10, 14, 19, 23, 59, 176)
-  end = c(4, 9, 13, 18, 22, 33, 83, 210)
-  name = c("muni", "blk", "blk_suffix", "lot", "lot_suffix", "qual", "address", "owner")
   
   con = file(filename)
   l = strsplit(readLines(con), "\n")
@@ -56,27 +58,48 @@ nj = nj.temp
 #nj$blk_suffix = gsub("^$|^ *$", "", nj$blk_suffix)
 #nj$lot_suffix = gsub("^$|^ *$", "", nj$lot_suffix)
 #nj$qual = gsub("^$|^ *$", "", nj$qual)
+nj$blk.raw = nj$blk
+nj$lot.raw = nj$lot
+nj$p.blk.raw = nj$p.blk
+nj$p.lot.raw = nj$p.lot
+
 nj$blk = as.character(as.numeric(nj$blk))
 nj$lot = as.character(as.numeric(nj$lot))
+nj$p.blk = as.character(as.numeric(nj$p.blk))
+nj$p.lot = as.character(as.numeric(nj$p.lot))
+
 nj$blk = gsub("( +)","", nj$blk)
 nj$lot = gsub("( +)", "", nj$lot)
-nj$blk_suffix = trimws(nj$blk_suffix)
-nj$lot_suffix = trimws(nj$lot_suffix)
-nj[which(nj$blk_suffix != ""),c("blk_suffix")] = paste(".", nj[which(nj$blk_suffix != ""),c("blk_suffix")], sep = "")
-nj$lot_suffix[which(nj$lot_suffix != "")] = paste(".", nj$lot_suffix[which(nj$lot_suffix != "")], sep = "")
+nj$p.blk = gsub("( +)","", nj$p.blk)
+nj$p.lot = gsub("( +)", "", nj$p.lot)
 
-nj$blk = paste(nj$blk, nj$blk_suffix, sep = "")
-nj$lot = paste(nj$lot, nj$lot_suffix, sep= "")
+nj$blk.s = trimws(nj$blk.s)
+nj$lot.s = trimws(nj$lot.s)
+nj$p.blk.s = trimws(nj$p.blk.s)
+nj$p.lot.s = trimws(nj$p.lot.s)
+nj$qual = trimws(nj$qual)
+nj$p.qual = trimws(nj$p.qual)
 
-drops = c("blk_suffix","lot_suffix")
-nj = nj[,!(names(nj) %in% drops)]
+nj$blk[which(nj$blk.s != "")] = paste(nj$blk[which(nj$blk.s != "")], ".", sep = "")
+nj$lot[which(nj$lot.s != "")] = paste(nj$lot[which(nj$lot.s != "")], ".", sep = "")
+nj$p.blk[which(nj$p.blk.s != "")] = paste(nj$p.blk[which(nj$p.blk.s != "")], ".", sep = "")
+nj$p.lot[which(nj$p.lot.s != "")] = paste(nj$p.lot[which(nj$p.lot.s != "")], ".", sep = "")
 
-View(nj[1:1000,])
+nj$blk = paste(nj$blk, nj$blk.s, sep = "")
+nj$lot = paste(nj$lot, nj$lot.s, sep = "")
+nj$p.blk = paste(nj$p.blk, nj$p.blk.s, sep = "")
+nj$p.lot = paste(nj$p.lot, nj$p.lot.s, sep = "")
+
+#drops = c("blk.s","lot.s")
+#nj = nj[,!(names(nj) %in% drops)]
+
+#View(nj[1:1000,])
 
 nj$lotqual = paste(nj$lot,nj$qual,sep = "")
+nj$p.lotqual = paste(nj$p.lot, nj$p.qual, sep = "")
 
-write.table(nj, file = "nj.csv", sep = ",", row.names = F)
-nj1 = read.csv("nj.csv")
+#write.table(nj, file = "nj.csv", sep = ",", row.names = F)
+#nj1 = read.csv("nj.csv")
 ## clean blk and lot data 
 
 blk = d3s$G
@@ -89,11 +112,11 @@ blkpattern2 = "(^\\d+[.]*\\d*)(\\s*)(((FKA)|(F/K/A)|(QUAL)|(AKA)|(A/K/A)|(FK)|(F
 blk1 = gsub(blkpattern1, "\\1.\\3", blk)
 blk2 = gsub(blkpattern2, "\\1", blk1)
 
-View(cbind(blk2, blk1, blk))
+#View(cbind(blk2, blk1, blk))
 blkn = blk2
 
 # clean lot$qual
-View(cbind(lot3, lot2, lot1, lot))
+#View(cbind(lot3, lot2, lot1, lot))
 lotpattern1 = "(F/K/A.*)|(AKA.*)|(A/K/A.*)|(FK.*)|(F/K.*)|(AND.*)|(&.*)|(THRU.*))|(:)|(QUAL)|(\\()|(\\))|[,]"
 lotpattern2 = "(^\\d+\\b)(\\s+)([0]\\d+.*\\b)"
 lotpattern3 = "(^\\d+\\b)(\\s+)(\\d+\\b)(\\s+)(\\d+\\b)"
@@ -107,15 +130,56 @@ lotn = gsub(" ", "", lot3)
 
 ## search blk
 
-apn = cbind.data.frame(d3s$J, blkn, lotn)
-names(apn) = c("muni","blk","lotnqual")
-apn$blk.n = rep(NA)
+apn = cbind(d3s$J, blkn, lotn)
+apn = as.data.frame(apn, stringsAsFactors = F)
+apn$blk.raw = rep(NA)
+apn$blk.suffix.raw = rep(NA)
+apn$lot.raw = rep(NA)
+apn$lot.suffix.raw = rep(NA)
+apn$qual = rep(NA)
+apn$address = rep(NA)
+apn = cbind(apn, d3s$D, d3s$F)
+
+
+
+#names(apn) = c("muni","blk","lotnqual")
+
+
+
 # apn = apply(apn, 2, as.character) #?why return a matrix
 
 f = function(x){
-  a = which(x$blk %in% nj[which(nj$muni == x$muni),]$blk)
-  x$blk.n[a] = as.character(x$blk[a])
+  x = as.character(x)
+  #a = x[2] %in% nj[which(nj$muni == x[1]),]$blk
+  which(nj$muni == x[1] & nj$blk == x[2] & nj$lotqual == x[3])
+  
+  ifelse(x[2] %in% nj[which(nj$muni == x[1]),]$blk, x[c(4,5)] <- nj[which(nj$muni == x[1] & nj$blk == x[2]),][1,c("blk.raw", "blk_suffix")], x[c(4, 5)] <- NA)
+  return(t(x))
+}
+
+f = function(x){
+  x = as.character(x)
+  #a = x[2] %in% nj[which(nj$muni == x[1]),]$blk
+  a = which(nj$muni == x[1] & nj$blk == x[2] & nj$lotqual == x[3])
+  b = which(nj$muni == x[1] & nj$p.blk == x[2] & nj$p.lotqual == x[3])
+  if (length(a) == 1) {
+    nj$blk.raw[a] -> x[4]
+    nj$blk.s[a] -> x[5]
+    nj$lot.raw[a] -> x[6]
+    nj$lot.s[a] -> x[7]
+    nj$qual[a] -> x[8]
+    nj$address[a] -> x[9]
+  } else if (length(b) == 1) {
+    nj$blk.raw[b] -> x[4]
+    nj$blk.s[b] -> x[5]
+    nj$lot.raw[b] -> x[6]
+    nj$lot.s[b] -> x[7]
+    nj$qual[b] -> x[8]
+    nj$address[b] -> x[9]
+  }
   return(x)
 }
 
-test = apply(apn, 1, f)
+test = apply(apn[1:300,], 1, f)
+test = t(test)
+View(test1)
